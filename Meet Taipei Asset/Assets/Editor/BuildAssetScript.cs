13384c8a14a5e16efca7fb0655bba4405a9d9b6f
DiosVan <dino6x6@gmail.com>
2018-01-30 23:54:43 +0800

    Initial commit

---
+++ Meet Taipei Asset/Assets/Editor/BuildAssetScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetScript : MonoBehaviour
+{
+	private static BuildTarget mTargetPlatform = EditorUserBuildSettings.activeBuildTarget;
+	public enum CompressionType
+	{
+		Uncompress,
+		LZMA,
+		LZ4
+	}
+
+	[MenuItem("Custom Editor/Build AssetBundles Uncompress")]
+	static void PackWindowAsset()
+	{
+		ExecCreateAssetBundles(CompressionType.Uncompress);
+	}
+
+	[MenuItem("Custom Editor/Build AssetBundles LZMA")]
+	static void PackAndroidAsset()
+	{
+		ExecCreateAssetBundles(CompressionType.LZMA);
+	}
+
+	[MenuItem("Custom Editor/Build AssetBundles LZ4")]
+	static void PackIosAsset()
+	{
+		ExecCreateAssetBundles(CompressionType.LZ4);
+	}
+
+	static void ExecCreateAssetBundles(CompressionType compressionType)
+	{
+		BuildAssetBundleOptions buildOptions = BuildAssetBundleOptions.None;
+		switch (compressionType)
+		{
+			case CompressionType.Uncompress:
+				buildOptions = BuildAssetBundleOptions.UncompressedAssetBundle;
+				break;
+			case CompressionType.LZMA:
+				break;
+			case CompressionType.LZ4:
+				buildOptions = BuildAssetBundleOptions.ChunkBasedCompression;
+				break;
+		}
+
+		// AssetBundle 的資料夾名稱及副檔名
+		string targetDir = "AssetBundles";
+		string extensionName = ".assetBundles";
+
+		//取得在 Project 視窗中選擇的資源(包含資料夾的子目錄中的資源)
+		Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+		string folderPath = string.Format("{0}/{1}", targetDir, mTargetPlatform.ToString());
+
+		//建立存放 AssetBundle 的資料夾
+		if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+
+		//Debug.Log(SelectedAsset.Length);
+
+		foreach (Object obj in SelectedAsset)
+		{
+			//資源檔案路徑
+			string sourcePath = AssetDatabase.GetAssetPath(obj);
+
+			// AssetBundle 儲存檔案路徑
+			string targetPath = string.Format("{0}{1}{2}", targetDir, Path.DirectorySeparatorChar, mTargetPlatform.ToString()) + Path.DirectorySeparatorChar + obj.name + extensionName;
+
+			//取代檔案
+			if (File.Exists(targetPath)) File.Delete(targetPath);
+
+			//
+			if (!(obj is GameObject) && !(obj is Texture2D) && !(obj is Material)) continue;
+
+		}
+
+		BuildPipeline.BuildAssetBundles(folderPath, buildOptions, mTargetPlatform);
+	}
+}
---
+++ Meet Taipei Asset/Assets/Script/LoadAssetHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadAssetHandler : MonoBehaviour
+{
+	public delegate void OnDownLoadProgress(float progress, bool isComplete, string error);
+
+	public static LoadAssetHandler Instance = null;
+
+	private Dictionary<string, AssetBundle> mBundleDict;
+
+	public static void Initial()
+	{
+		if (null == Instance)
+		{
+			var go = new GameObject("AssetHandler", typeof(LoadAssetHandler));
+			DontDestroyOnLoad(go);
+			Instance = go.GetComponent<LoadAssetHandler>();
+
+			Instance.mBundleDict = new Dictionary<string, AssetBundle>();
+		}
+	}
+
+	public void DownloadAsset()
+	{
+		if (null == Instance)
+			return;
+
+		StartCoroutine(LoadBundleList());
+	}
+
+	private IEnumerator LoadBundleList()
+	{
+		while (!Caching.ready)
+			yield return null;
+
+		string basePath = string.Format("file://{0}/AssetBundles/StandaloneWindows/", Application.dataPath);
+
+		string bundleName = "StandaloneWindows";
+		string url = basePath + bundleName;
+		string manifestURL = url + ".manifest";
+		//
+		url += ((url.Contains("?")) ? "&" : "?") + "t=" + DateTime.Now.ToString("yyyyMMddHHmmss");
+		manifestURL += ((manifestURL.Contains("?")) ? "&" : "?") + "t=" + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+		UnityWebRequest wwwManifest = UnityWebRequest.Get(manifestURL);
+		// 
+		yield return wwwManifest.Send();
+
+		// 
+		uint latestCRC = 0;
+		if (string.IsNullOrEmpty(wwwManifest.error))
+		{
+			// 
+			string[] lines = wwwManifest.downloadHandler.text.Split(new string[] { "CRC: " }, StringSplitOptions.None);
+			latestCRC = uint.Parse(lines[1].Split(new string[] { "\n" }, StringSplitOptions.None)[0]);
+		}
+		else
+			Debug.Log(bundleName + ".manifest has not found.");
+
+		Debug.Log("latestCRC :" + latestCRC);
+	}
+
+	private IEnumerator LoadAs()
+	{
+		string path = string.Format("file://{0}/AssetBundles/{1}", Application.dataPath, "background");
+		string urlPath = "https://www.dropbox.com/s/14t9wbb6u67n4dw/background?dl=1";
+
+		var request = UnityWebRequest.GetAssetBundle(urlPath);
+		
+		var requestHandler = request.Send();
+
+		while (!requestHandler.isDone)
+		{
+			yield return null;
+			Debug.Log(requestHandler.progress);
+			// TODO: Show progress bar, op.progress
+		}
+
+		var assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+
+		//request.Dispose();
+		
+
+		string[] strr = assetBundle.GetAllAssetNames();
+
+		for (int i = 0; i < strr.Length; i++)
+			Debug.Log("??:" + strr[i]);
+	}
+}
