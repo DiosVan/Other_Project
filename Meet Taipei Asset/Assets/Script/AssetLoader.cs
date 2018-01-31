45c02c2c189775876b5f5c9bb8626f7e7b3c0765
DiosVan <dino6x6@gmail.com>
2018-01-31 23:39:08 +0800

    調整架構

---
+++ Meet Taipei Asset/Assets/Script/AssetLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetLoader : ILoader
+{
+	string IProgress.TargetName { get; }
+
+	bool IProgress.IsFailed { get; }
+
+	bool IProgress.IsDone { get; }
+
+	float IProgress.Progress { get; }
+
+	object IEnumerator.Current { get; }
+
+	void IDisposable.Dispose()
+	{
+		throw new NotImplementedException();
+	}
+
+	void ILoader.DownloadFinish()
+	{
+		throw new NotImplementedException();
+	}
+
+	bool IEnumerator.MoveNext()
+	{
+		throw new NotImplementedException();
+	}
+
+	void IEnumerator.Reset()
+	{
+		throw new NotImplementedException();
+	}
+}
--- Meet Taipei Asset/Assets/Script/DownLoadPorgress.cs
+++ Meet Taipei Asset/Assets/Script/DownLoadPorgress.cs
@@ -3,21 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DownLoadPorgress : IProgress
+public class DownLoadPorgress : MonoBehaviour
 {
-	public object Current { get { throw new NotImplementedException(); } }
-
-	public bool IsDone { get { throw new NotImplementedException(); } }
-
-	public bool IsFailed { get { throw new NotImplementedException(); } }
-
-	public float Progress { get { throw new NotImplementedException(); } }
-
-	public string TargetName { get { throw new NotImplementedException(); } }
-
-	public void Dispose() { throw new NotImplementedException(); }
-
-	public bool MoveNext() { throw new NotImplementedException(); }
-
-	public void Reset() { throw new NotImplementedException(); }
+	
 }
